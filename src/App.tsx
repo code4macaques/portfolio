a0e3eda48115b6ae5245025946f88345d373fa5e
Jesus Alejandro Tirado Sanchez <tiradoja+GHCO@globalhitss.com>
2026-03-16 22:09:40 -0700

    fourth commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@
  */
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import GoogleAnalytics from "./components/GoogleAnalytics";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Skills from "./sections/Skills";
@@ -26,6 +27,7 @@ export default function App() {
 
   return (
     <LanguageProvider>
+      <GoogleAnalytics />
       <div className="bg-background-light font-sans text-text-dark transition-colors duration-300 dark:bg-dark-bg dark:text-dark-text">
         {/* Sticky navigation bar */}
         <Navbar theme={theme} onToggleTheme={toggleTheme} />
